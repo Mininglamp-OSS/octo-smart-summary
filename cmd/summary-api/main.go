cc50c29f61db8f74f1b42a88542f5bcebe097196
lxy <liuxinyu.a@mininglamp.com>
2026-07-09 15:08:16 +0800

    feat(summary): add user topic templates

--- cmd/summary-api/main.go
+++ cmd/summary-api/main.go
@@ -95,7 +95,7 @@ func main() {
 	}
 
 	// Public API server
-	publicRouter := router.SetupPublic(summaryDB, imDB, hub, authResolver, cfg.WorkerTriggerURL, cfg.CandidateQueryLimit, cfg.FeatureTeamSchedule)
+	publicRouter := router.SetupPublic(summaryDB, imDB, hub, authResolver, cfg.WorkerTriggerURL, cfg.CandidateQueryLimit, cfg.FeatureTeamSchedule, cfg.SummaryCustomTemplateLimit)
 	publicSrv := &http.Server{
 		Addr:    ":" + cfg.APIPort,
 		Handler: publicRouter,
